4c3053d7a173c0df467f7a5ae4cf5904b3741cb0
alok kumar0421 <aloknokha01@gmail.com>
2026-02-08 23:19:54 +0530

    add colon

--- 2_login_page/app.js
+++ 2_login_page/app.js
@@ -4,13 +4,13 @@ const app=express()
 
 app.get("/",(req,res)=>{
     res.render("login.ejs");
-})
+});
 
 app.get("/login",(req,res)=>{
     res.render("login");
-})
+});
 
 const port=3000
 app.listen(port,()=>{
     console.log(`app is listen at ${port}`)
-})
+});
